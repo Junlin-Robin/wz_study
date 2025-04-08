6bfb641dd8a576332c89b40c91e7b98326e41db2
WJL <347258143@qq.com>
2025-04-09 00:12:38 +0800

    init: 项目再此初始化

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'dumi';
 
+import favicon from './src/assets/favicon';
+
 export default defineConfig({
   themeConfig: {
     name: '学狂',
-    footer: '学完狂干三碗大米饭<br/>项目搭建 | 技术支持 <a href="https://github.com/Junlin-Robin" target="_blank">@Robin</a><br />文档编写 | 协助开发  <a href="https://github.com/Geistesblitz7" target="_blank">@Geistesblitz</a>'
+    footer: 'Just Crazy | 学完狂干三碗大米饭<br/>Copyright © <a href="https://github.com/Junlin-Robin" target="_blank">Robin</a> and <a href="https://github.com/Geistesblitz7" target="_blank">Geistesblitz</a> 2025-present',
+    nprogress: true,
+    socialLinks: {
+      github: 'https://github.com/Junlin-Robin/wz_study',
+    }
   },
   base: '/wz_study/',
-  publicPath: '/wz_study/'
+  publicPath: '/wz_study/',
+  favicons: [favicon],
 });
---
+++ docs/for-master/language/java/demo.md
@@ -0,0 +1,11 @@
+---
+nav:
+  second: 编程语言
+  order: 3
+group:
+  title: Java
+  order: 1
+title: 学习文档
+---
+
+### Java 学习文档
